a68e870ab2d48269dae1fa7ee073d465bdf22f45
Pavel Kosarev <kosarev.pg@gmail.com>
2025-11-25 18:10:04 +0300

    Fix pydantic version to be compatible with aiogram 3.1.1

--- back/build.sh
+++ back/build.sh
@@ -4,6 +4,9 @@ set -e
 # Обновляем pip
 pip install --upgrade pip setuptools wheel
 
-# Устанавливаем зависимости, предпочитая бинарные пакеты
+# Устанавливаем pydantic с предкомпилированными wheels (совместим с aiogram 3.1.1)
+pip install --prefer-binary "pydantic>=2.1.1,<2.4"
+
+# Устанавливаем остальные зависимости
 pip install --prefer-binary -r requirements.txt
 
